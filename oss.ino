6b735f7e3ef9ab73d8b07b1bc939335d0de493d9
Taewon Kang <itschool@itsc.kr>
2016-01-16 23:11:06 +0900

    Update oss.ino
    모터 가감속 조절, 딜레이 수정

--- oss.ino
+++ oss.ino
@@ -1,21 +1,22 @@
 #include <Servo.h> 
 
-int motorpin1 = 3;                 
-int motorpin2 = 4;  
+int motorpin1 = 3;                
 
-int echoPin = 6;
-int trigPin = 5;
-
+int motorpin2 = 4;                 
+ int echoPin = 13;
+int trigPin = 12;
 int servoPin = 9;
 Servo servo; 
 int angle = 0; 
+int sw = 0, i;
 
-void setup () 
-{
-  pinMode(motorpin1,OUTPUT); 
-  pinMode(motorpin2,OUTPUT); 
+void setup () {
+
+  pinMode(motorpin1,OUTPUT);    
+
+  pinMode(motorpin2,OUTPUT);      
   servo.attach(servoPin); 
-  Serial.begin(9600);
+Serial.begin(9600);
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -25,26 +26,47 @@ void loop () {
 
   digitalWrite(motorpin1,LOW);
   digitalWrite(motorpin2,HIGH);
-  float duration, distance;
+    float duration, distance;
   digitalWrite(trigPin, HIGH);
   delay(10);
   digitalWrite(trigPin, LOW);
+
   
   duration = pulseIn(echoPin, HIGH); 
   distance = ((float)(340 * duration) / 10000) / 2;
 
   Serial.print(distance);
   Serial.println("cm");
-  
-  delay(500);
-    for(angle = 0; angle < 180; angle++) 
-  { 
-    servo.write(angle); 
-    delay(15); 
-  } 
-  for(angle = 180; angle > 0; angle--) 
-  { 
-    servo.write(angle); 
-    delay(15); 
-  } 
+  delay(5);
+
+  motor(distance);
 }
+
+void motor(float distance)
+{
+  if(distance > 15.000)
+  {
+    if(sw >= 0)
+    {
+        servo.write(180); 
+        delay(30);
+        sw = 180;
+    }
+    if(sw <= 180)
+    {
+        servo.write(0); 
+        delay(30);
+        sw = 0;
+    }
+  }
+  else
+  {
+    if(distance < 12) analogWrite(motorpin1, 255);
+    if(distance < 11) analogWrite(motorpin1, 180);
+    if(distance < 10) analogWrite(motorpin1, 120);
+    if(distance < 9) analogWrite(motorpin1, 80);
+    if(distance < 6) analogWrite(motorpin1, 50);
+    if(distance < 3) analogWrite(motorpin1, 20);
+  }
+}
+
